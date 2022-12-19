2be9df352e908adb3f0eb357ef7d67ef184e62b6
g3n0m <x9057075636@gmail.com>
2022-12-19 20:25:02 +0300

    Initial Commit

---
+++ ReactFrontend/src/components/Main.jsx
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+
+function Main() {
+    const [isLoading, setLoading] = useState(true);  // Флаг готовности результата axios
+    const [categories, setCategories] = useState();
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/categories/").then(res => {
+            setCategories(res.data);
+            setLoading(false);
+        });
+    }, []);
+
+    // Если флаг isLoading = false то выводим "Loading..."
+    if (isLoading) {
+        return <h1>Loading...</h1>;
+    }
+
+    // Иначе выводим полученные из axios данные
+    return (
+        <div className="button">
+            {categories.map((name) => (
+                <a key={name.categoryType} className="s" href={`/category/${name.categoryType}`}>{name.categoryType}</a>
+            ))}
+        </div>
+    );
+}
+
+export default Main
